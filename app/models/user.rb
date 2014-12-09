9b82223820ada513cabc2ecfcd7679124e56e6cf
fishermanswharff <fishermanswharff@github.com>
2014-12-08 21:49:41 -0500

    Autopush JWAPI Master Branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,16 @@ class User < ActiveRecord::Base
   enum role: [:admin,:generic]
   has_secure_password
 
+  has_attached_file :avatar, 
+                    :styles => { :medium => "300x>", :thumb => "100x>" },
+                    :bucket => 'dubya-blog-bucket',
+                    :s3_credentials => {
+                      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                    }
+
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   def self.create_from_omniauth(params)
     attributes = {
       username: params['info']['name'],
