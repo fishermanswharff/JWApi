b2920e3e351d2cade79058c78ed4c5241015e6a2
fishermanswharff <fishermanswharff@github.com>
2014-12-09 12:38:12 -0500

    Autopush JWAPI Master Branch

--- config/application.rb
+++ config/application.rb
@@ -31,10 +31,10 @@ module Jwapi
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.middleware.insert_before "ActionDispatch::Static", "Rack::Cors" do
+    config.middleware.use Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
       end
     end
   end
