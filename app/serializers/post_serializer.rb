b11b423258602f0178df32e49c10c52350c5251b
fishermanswharff <fishermanswharff@github.com>
2014-12-09 23:03:59 -0500

    Autopush JWAPI Master Branch

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :categories, :user, :created_at, :images
+  attributes :id, :title, :body, :categories, :user, :created_at, :image, :images
   url [:user, :post]
 
   def categories
@@ -10,16 +10,21 @@ class PostSerializer < ActiveModel::Serializer
     object.user.username
   end
 
+  def image
+    object.image.url(:medium)
+  end
+
   def created_at
     p object.created_at.to_s(:rfc822)
   end
 
-  def attributes(options=nil)
-    object.attributes.merge!({images: get_image_urls})
+  def images
+    get_image_urls
   end
 
   private 
   def get_image_urls
-    object.image.styles.map { |style| object.image.url(style[0]) }
+    styles = object.image.styles.map { |style| style[0] }
+    styles.map { |style| object.image.url(style) }
   end
 end
