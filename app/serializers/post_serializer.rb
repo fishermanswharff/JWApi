2403b70267de100af923e5ac289443d7c11d0e59
fishermanswharff <fishermanswharff@github.com>
2014-12-11 14:15:40 -0500

    Autopush JWAPI Master Branch

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,6 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :categories, :user, :created_at, :image, :images
+  attributes :id, :title, :body, :categories, :user, :created_at #, :image, :images
+  has_many :images
   url [:user, :post]
 
   def categories
@@ -10,21 +11,7 @@ class PostSerializer < ActiveModel::Serializer
     object.user.username
   end
 
-  def image
-    object.image.url(:medium)
-  end
-
   def created_at
     p object.created_at.to_s(:rfc822)
-  end
-
-  def images
-    get_image_urls
-  end
-
-  private 
-  def get_image_urls
-    styles = object.image.styles.map { |style| style[0] }
-    styles.map { |style| object.image.url(style) }
   end
 end
