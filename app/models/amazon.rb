d7d72a69d666642a8f8fb4e3eabca2438245bf67
fishermanswharff <fishermanswharff@github.com>
2014-12-13 01:34:42 -0500

    Autopush JWAPI Master Branch

--- app/models/amazon.rb
+++ app/models/amazon.rb
@@ -1,8 +1,6 @@
 require 'active_support/core_ext'
 
 class Amazon # no inheritance here bc this class does not connect to Postgres
-  
-
   def self.get_s3_upload_key
     bucket = ENV['S3_BUCKET_NAME']
     access_key = ENV['AWS_ACCESS_KEY_ID']
@@ -19,16 +17,25 @@ class Amazon # no inheritance here bc this class does not connect to Postgres
         {'acl': '#{acl}'},
         ['starts-with', '$key', '#{key}'],
         ['content-length-range', 1, #{max_filesize}],
-        {x-amz-algorithm: 'HMAC-SHA1'},
-        {x-amz-credential: ""},
-        {}
-
-      ]}").gsub(/\n| |\r/, '')
-    signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), secret, policy)).gsub(/\n| |\r/, '')
+        {x-amz-algorithm: 'HMAC-SHA256'},
+        {x-amz-credential: #{access_key}/#{Time.now.strftime('%Y-%m-%d').gsub(/\-/,'')}/us-east-1/s3/aws4_request },
+        {x-amz-date: 5.minutes.from_now.utc.iso8601}
+      ]}").gsub(/\n|\r/, '')
+    date_key = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), "AWS4" + secret, "20141213")).gsub(/\n| |\r/,'')
+    date_region_key = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), date_key, "us-east-1")).gsub(/\n| |\r/,'')
+    date_region_service_key = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), date_region_key, "s3")).gsub(/\n| |\r/,'')
+    signing_key = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), date_region_service_key, "aws4_request")).gsub(/\n| |\r/,'')
+    
+    signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), signing_key, policy)).gsub(/\n| |\r/, '')
+    signature = CGI.escape(signature)
     return {access_key: access_key, key: key, policy: policy, signature: signature, sas: sas, bucket: bucket, acl: acl, expiration: expiration}
   end
 
 end
+
+
+
+# Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), <option1>, <option2>)).gsub(/\n| |\r/,'')
 
 
 # def self.get_s3_upload_key
