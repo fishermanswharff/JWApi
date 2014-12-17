f4a859e3a208be4096ef07a9551a4b2972f590af
fishermanswharff <fishermanswharff@github.com>
2014-12-16 22:26:08 -0500

    Autopush JWAPI Master Branch

--- spec/models/amazon_spec.rb
+++ spec/models/amazon_spec.rb
@@ -6,8 +6,4 @@ describe 'Amazon',type: :model do
   before(:all) do
     @instance = Amazon.get_s3_upload_key
   end
-
-  it 'should return valid json' do
-    expect(@instance.class).to be Hash
-  end
 end
