b77e3167f6ebad9cffc7a64aed99b26a09049082
fishermanswharff <fishermanswharff@github.com>
2014-12-16 22:59:23 -0500

    Autopush JWAPI Master Branch

--- config/initializers/gmail_variables.rb
+++ config/initializers/gmail_variables.rb
@@ -1,7 +1,7 @@
-module AWSEnvironmentVariables
+module GmailEnvironmentVariables
   class Application < Rails::Application
     config.before_configuration do
-      env_file = Rails.root.join("config", 'aws.yml').to_s
+      env_file = Rails.root.join("config", 'gmail.yml').to_s
       if File.exists?(env_file)
         YAML.load_file(env_file)[Rails.env].each do |key, value|
           ENV[key.to_s] = value
