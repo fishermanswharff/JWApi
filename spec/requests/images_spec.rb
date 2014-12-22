2747f20266100adf5708e83d549aefdbf57f966b
fishermanswharff <fishermanswharff@github.com>
2014-12-22 13:54:14 -0500

    Autopush JWAPI Master Branch

--- spec/requests/images_spec.rb
+++ spec/requests/images_spec.rb
@@ -6,6 +6,16 @@ describe 'Images API Endpoint' do
   before(:all) do
     Post.delete_all
     Image.delete_all
+    User.delete_all
+    @user = User.create({
+      first_name:"Jason",
+      last_name:"Wharff",
+      username:"jasonwharff",
+      role:"admin",
+      email:"fishermanswharff@mac.com",
+      password_digest:"$2a$10$qA2kdO7MOlWkAUIUQWhNtuzlIZScAs69QrTHxn13BM9i6jUUuLbfS",
+      token:"9cbaf8c82925426c869ecfc4b610c8a6",
+    })
     @post = FactoryGirl.create(:post)
     @images = FactoryGirl.create_list(:image,25)
     @images.map { |image| @post.images << image }
@@ -18,11 +28,21 @@ describe 'Images API Endpoint' do
   end
 
   describe '#create' do
-
+    it 'should add the image to the post''s images ' do
+      post "/posts/#{@post.id}/images",
+        { image: 
+          { url: 'https://s3.amazonaws.com/dubya-blog-bucket/uploads/eb00f33c-b80e-4e72-ada4-36bdc24f551b',
+            post_id: @post.id
+           }
+        }.to_json,
+        { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s, 'HTTP_AUTHORIZATION' => '9cbaf8c82925426c869ecfc4b610c8a6' }
+      expect(response).to be_success
+      expect(response.content_type).to be Mime::JSON
+    end
   end
 
   describe '#update' do
-    
+
   end
 
   describe '#destroy' do
