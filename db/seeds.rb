3baac2dd6c00e133ac4134293cfb8f79acc1fc28
fishermanswharff <fishermanswharff@github.com>
2015-02-17 11:40:11 -0500

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
 User.delete_all
 Post.delete_all
-Category.delete_all
+# Category.delete_all
+
 
 ActiveRecord::Base.establish_connection
 ActiveRecord::Base.connection.tables.each do |table|
   next if table == 'schema_migrations'
   ActiveRecord::Base.connection.execute("TRUNCATE #{table}")
 end
+
 
 users = User.create([
   {
@@ -65,7 +67,27 @@ posts = Post.create([
   { title: "Title 1", body: "Curabitur blandit tempus porttitor. Aenean lacinia bibendum nulla sed consectetur. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec ullamcorper nulla non metus auctor fringilla.", user_id: users[0].id },
   { title: "Title 2", body: "Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis risus eget urna mollis ornare vel eu leo. Maecenas sed diam eget risus varius blandit sit amet non magna. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus.", user_id: users[0].id },
   { title: "Title 3", body: "Maecenas faucibus mollis interdum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod. Nullam quis risus eget urna mollis ornare vel eu leo. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.", user_id: users[0].id },
-  { title: "Title 4", body: "Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.", user_id: users[0].id }
+  { title: "Title 4", body: "Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.", user_id: users[0].id },
+  { title: "Title 5", body: "Cras mattis consectetur purus sit amet fermentum.", user_id: users[0].id },
+  { title: "Title 6", body: "Cras justo odio, dapibus ac facilisis in, egestas eget quam.", user_id: users[0].id },
+  { title: "Title 7", body: "Sed posuere consectetur est at lobortis.", user_id: users[0].id },
+  { title: "Title 8", body: "Etiam porta sem malesuada magna mollis euismod.", user_id: users[0].id },
+  { title: "Title 9", body: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", user_id: users[0].id },
+  { title: "Title 10", body: "Cras justo odio, dapibus ac facilisis in, egestas eget quam.", user_id: users[0].id },
+  { title: "Title 11", body: "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.", user_id: users[0].id },
+  { title: "Title 12", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", user_id: users[0].id },
+  { title: "Title 13", body: "Curabitur blandit tempus porttitor.", user_id: users[0].id },
+  { title: "Title 14", body: "Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.", user_id: users[0].id },
+  { title: "Title 15", body: "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.", user_id: users[0].id },
+  { title: "Title 16", body: "Curabitur blandit tempus porttitor.", user_id: users[0].id },
+  { title: "Title 17", body: "Nulla vitae elit libero, a pharetra augue.", user_id: users[0].id },
+  { title: "Title 18", body: "Aenean lacinia bibendum nulla sed consectetur.", user_id: users[0].id },
+  { title: "Title 19", body: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", user_id: users[0].id },
+  { title: "Title 20", body: "Morbi leo risus, porta ac consectetur ac, vestibulum at eros.", user_id: users[0].id },
+  { title: "Title 21", body: "Aenean lacinia bibendum nulla sed consectetur.", user_id: users[0].id },
+  { title: "Title 22", body: "Donec id elit non mi porta gravida at eget metus.", user_id: users[0].id },
+  { title: "Title 23", body: "Donec sed odio dui.", user_id: users[0].id },
+  { title: "Title 24", body: "Integer posuere erat a ante venenatis dapibus posuere velit aliquet.", user_id: users[0].id }
 ])
 
 images = Image.create([
@@ -86,16 +108,65 @@ images = Image.create([
 
 posts[0].categories << Category.find_by(name:"Development")
 posts[0].categories << Category.find_by(name:"Ruby")
+posts[0].categories << Category.find_by(name:'Javascript')
+posts[0].categories << Category.find_by(name:'TIL')
+
 posts[1].categories << Category.find_by(name:"Development")
 posts[1].categories << Category.find_by(name:"Ruby")
 posts[1].categories << Category.find_by(name:"Computer Science")
 posts[1].categories << Category.find_by(name:"TIL")
+
 posts[2].categories << Category.find_by(name:"Development")
+posts[2].categories << Category.find_by(name:"Graphic Design")
+posts[2].categories << Category.find_by(name:"XML")
+posts[2].categories << Category.find_by(name:"TIL")
+
 posts[3].categories << Category.find_by(name:"Development")
+posts[3].categories << Category.find_by(name:"Photography")
+posts[3].categories << Category.find_by(name:"Ruby")
+posts[3].categories << Category.find_by(name:"Illustration")
+
 posts[4].categories << Category.find_by(name:"Development")
+posts[4].categories << Category.find_by(name:"Data Viz")
+posts[4].categories << Category.find_by(name:"Computer Science")
+posts[4].categories << Category.find_by(name:"Travel")
+
 posts[5].categories << Category.find_by(name:"Development")
+posts[5].categories << Category.find_by(name:"Hobbies")
+posts[5].categories << Category.find_by(name:"Testing")
+posts[5].categories << Category.find_by(name:"SQL")
+
 posts[6].categories << Category.find_by(name:"Development")
-posts[7].categories << Category.find_by(name:"Development")
+posts[6].categories << Category.find_by(name:"Ruby")
+posts[6].categories << Category.find_by(name:"jQuery")
+posts[6].categories << Category.find_by(name:"XML")
+
+posts[7].categories << Category.find_by(name:"Javascript")
+posts[8].categories << Category.find_by(name:"CSS")
+posts[9].categories << Category.find_by(name:"Travel")
+posts[10].categories << Category.find_by(name:"SQL")
+posts[11].categories << Category.find_by(name:"Infographics")
+posts[12].categories << Category.find_by(name:"TIL")
+posts[13].categories << Category.find_by(name:"Infographics")
+posts[14].categories << Category.find_by(name:"SQL")
+posts[15].categories << Category.find_by(name:"Graphic Design")
+posts[16].categories << Category.find_by(name:"AngularJS")
+posts[17].categories << Category.find_by(name:"Design")
+posts[18].categories << Category.find_by(name:"Ruby")
+posts[19].categories << Category.find_by(name:"Javascript")
+posts[20].categories << Category.find_by(name:"Illustration")
+posts[21].categories << Category.find_by(name:"Travel")
+posts[22].categories << Category.find_by(name:"Hobbies")
+posts[23].categories << Category.find_by(name:"HTML")
+posts[24].categories << Category.find_by(name:"Photography")
+posts[25].categories << Category.find_by(name:"TIL")
+posts[26].categories << Category.find_by(name:"Ruby")
+posts[27].categories << Category.find_by(name:"TIL")
+posts[28].categories << Category.find_by(name:"CSS")
+posts[29].categories << Category.find_by(name:"TIL")
+posts[30].categories << Category.find_by(name:"Data Viz")
+posts[31].categories << Category.find_by(name:"TIL")
+posts[32].categories << Category.find_by(name:"TIL")
 
 images.each { |image| posts[12].images << image }
 
